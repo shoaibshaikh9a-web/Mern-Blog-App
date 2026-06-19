1b7af5ec9b393f363bd3554a730fb8b81b46ab39
ShoaibShaikh <shoaibshaikh9a@gmail.com>
2026-06-19 18:00:37 +0530

    updated env file for variables and fixed render port for5000

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
 
-mongoose.connect('mongodb://shoaibshaikhUser:ShoaibShaikh123@ac-vqx3cmx-shard-00-00.gwj02wu.mongodb.net:27017,ac-vqx3cmx-shard-00-01.gwj02wu.mongodb.net:27017,ac-vqx3cmx-shard-00-02.gwj02wu.mongodb.net:27017/?ssl=true&replicaSet=atlas-27ggn0-shard-0&authSource=admin&appName=Cluster0'
+mongoose.connect(process.env.MONGO_URI
 
 ).then(() => console.log('connected to mongodb'))
 .catch((e)=>console.log(e));
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express= require('express');
 const cors= require('cors');
 const blogRouter= require('./route/blog-route')
 require('./db');
+const PORT = process.env.PORT || 5000
 
 const app= express()
 app.use(cors())
@@ -13,4 +14,4 @@ app.use("/api",(req,res)=>{
     res.send("hello World");
 })
 
-app.listen(5000,()=>console.log('App is running at 5000...'))
+app.listen(PORT,()=>console.log('App is running at 5000...'))
