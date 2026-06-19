4fc0a868f21a2b11a38efe206cc332c2fac16f7c
ShoaibShaikh <shoaibshaikh9a@gmail.com>
2026-06-19 19:56:02 +0530

    small correction update at db uri

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
-
+console.log(process.env);
 mongoose.connect(process.env.MONGO_URI
 
 ).then(() => console.log('connected to mongodb'))
